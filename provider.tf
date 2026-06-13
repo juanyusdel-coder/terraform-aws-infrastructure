d2bcf654928011dcad67b8a661e559a869947410
Yusdel Concepcion <juanyusdel@gmail.com>
2026-06-13 01:28:20 -0300

    docs: add pipeline status badge to README

--- provider.tf
+++ provider.tf
@@ -3,12 +3,16 @@
 ###############################################################
 
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.9.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.50"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
     }
   }
 
